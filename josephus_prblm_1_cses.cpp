70aec9afcbd8ddac6d9cc9bb3351c7df11e9c28f
ashokbob9989 <98094230+ashokbob9989@users.noreply.github.com>
2022-12-17 16:36:43 +0530

    Add files via upload

---
+++ josephus_prblm_1_cses.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long
+
+void do_it()
+{
+    ll n;
+    cin >> n;
+    ll k=1;
+    ordered_set os;
+    for (ll i = 1; i <= n; i++)
+        os.insert(i);
+
+    ll pos = 0;
+    while (os.size() > 1)
+    {
+        ll xx = os.size();
+        pos = (pos + k) % xx;
+
+        cout << *(os.find_by_order(pos)) << " ";
+
+        os.erase(*(os.find_by_order(pos)));
+
+        pos %= xx;
+    }
+    cout << *(os.find_by_order(0)) << endl;
+}
+int32_t main()
+{
+    do_it();
+    return 0;
+}
