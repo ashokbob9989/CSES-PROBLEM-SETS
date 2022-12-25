39aa747a31e6a458bec8a98788f57a430d7b348e
ashokbob9989 <98094230+ashokbob9989@users.noreply.github.com>
2022-12-25 18:00:15 +0530

    Add files via upload

---
+++ message_route_cses.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+#define ld double
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ff first
+#define ss second
+#define ins insert
+#define vll vector <ll>
+#define vvll vector <vll>
+#define vbool vector <bool>
+#define pll pair <ll,ll>
+#define vpll vector <pll>
+#define set_bits __builtin_popcountll
+#define sz(v) (int)v.size()
+#define allin(v) v.begin(), v.end()
+#define allinr(v) v.rbegin(), v.rend()
+#define desc() greater <ll>()
+#define jaldi ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+template<class T> using oset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<class key, class value> using omap = tree <key,value,less<key>,rb_tree_tag,tree_order_statistics_node_update>;
+const int ebl = 1e9+7;
+const int newmod = 998244353;
+const int N = 2e5+7;
+const ll inf = 1e18;
+const ld eps = 1e-9;
+#define PI 3.141592653589793238462
+#define modinv2 500000004
+ll gcd(ll a, ll b) { while (b) {a %= b; swap(a,b);} return a; }
+ll lcm(ll a, ll b) { ll g=gcd(a,b); ll res=a*(b/g); return res; }
+ll extended_gcd(ll a, ll b, ll &x, ll &y) { if (b==0) { x=1; y=0; return a; } ll x1,y1; ll g=extended_gcd(b,a%b,x1,y1); x=y1; y=x1-y1*(a/b); return g; }
+ll binExp(ll a, ll b, ll m=ebl) { a = a % m; ll res = 1; while (b) { if (b&1) { res=(res * a) % m; } a=(a * a) % m; b>>=1; } return res; }
+ll mod_inv(ll a, ll m=ebl) { a = a % m; return binExp(a,m-2,m); }
+ll mod_add(ll a, ll b, ll m=ebl) { a = a % m; b = b % m; return (((a + b) % m) + m) % m; }
+ll mod_sub(ll a, ll b, ll m=ebl) { a = a % m; b = b % m; return (((a - b) % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m=ebl) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+ll mod_div(ll a, ll b, ll m=ebl) { a = a % m; ll binv = mod_inv(b,m); return (((a * binv) % m) + m) % m; }
+ll sqrtll(ll n) { ll lo=0,hi=1e9+7; while (hi-lo>1) { ll m=(hi+lo)/2; if (m*m<=n) { lo=m; } else { hi=m-1; }} if (hi*hi<=n) { return hi; }
+return lo; }
+ld sqrtld(ll n) { ld lo=0,hi=1e9+7; while (hi-lo>eps) { ld m=(hi+lo)/2; if ((n-m*m)>eps) { lo=m; } else { hi=m-eps; }} return lo; }
+
+vll adj[N];
+vbool vis(N,0);
+vll par(N,-1);
+
+void bfs(ll ver)
+{
+    vis[ver]=true;
+    queue<ll>q;
+    q.push(ver);
+
+    while(!q.empty())
+    {
+        ll temp=q.front();
+        q.pop();
+
+        for(auto child : adj[temp])
+        {
+            if(!vis[child])
+            {
+                vis[child]=true;
+                par[child]=temp;
+                q.push(child);
+            }
+        }
+    }
+
+}
+
+int32_t main()
+{
+    jaldi;
+    ll n,m;
+    cin>>n>>m;
+
+    for(ll i=0;i<m;i++)
+    {
+        ll x,y;
+        cin>>x>>y;
+        adj[x].pb(y);
+        adj[y].pb(x);
+    }
+    bfs(1);
+
+    if(par[n]==-1)
+    {
+        cout<<"IMPOSSIBLE"<<endl;
+        return 0;
+    }
+
+    ll from=n;
+    stack<ll>st;
+
+    while(from!=-1)
+    {
+        st.push(from);
+        from=par[from];
+    }
+    cout<<st.size()<<endl;
+
+    while(!st.empty())
+    {
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+
+}
